faec8111cfddc215e6b06fddec549c94b29d1439
Nahuel Ramos <47453696+Tanos9@users.noreply.github.com>
2023-02-11 03:38:02 -0300

    logout button and logged user info added to toolbar

--- src/app/shared/layout/header/header.component.ts
+++ src/app/shared/layout/header/header.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AuthService } from 'src/app/auth/services/auth.service';
+import { authenticatedUserSelector } from 'src/app/auth/store/auth.actions';
+import { AppState } from 'src/app/core/models/app-state.model';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy{
+  user: any;
+  
+  constructor(
+    public readonly authService: AuthService,
+    private readonly _store: Store<AppState>) {
+  }
+
+  ngOnInit(): void {
+    this._store.select(authenticatedUserSelector).subscribe((user) => {
+      this.user = user;
+    });
+  }
+
+  ngOnDestroy(): void {}
 
 }
